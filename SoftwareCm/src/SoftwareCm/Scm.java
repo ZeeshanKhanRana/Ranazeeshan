7c939363faa28e88c69b14502892064cbfa52fae
ZeeshanKhanRana <MuhammadZeeshanrana1240@gmail.com>
2016-04-25 17:41:18 +0500

    You have not changed yet

---
+++ SoftwareCm/src/SoftwareCm/Scm.java
@@ -0,0 +1,14 @@
+package SoftwareCm;
+
+public class Scm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello world");
+	System.out.println("Hi i m in");
+	
+	
+	}
+
+}
